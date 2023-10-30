7cfb47ab83151a30700ab65c338f9a06c70a01d6
Oleksiy Hryshchenko <greeschenko@gmail.com>
2023-10-30 23:03:28 +0200

    work for element connections

--- modelMind.go
+++ modelMind.go
@@ -96,18 +96,69 @@ func deleteCellListHandle(cc []Cell, id string, idlistres []string, idindex int)
 	return cc
 }
 
-func (m *Mind) Extend(s Cell, parentid string) Cell {
-	s.genID(parentid)
+func (m *Mind) Extend(newcell Cell, parentid string) Cell {
+	newcell.genID(parentid)
 	if parentid == "0" {
-		*m = append(*m, s)
+		*m = append(*m, newcell)
 	} else {
-		t := m.Find("id", parentid, true)
-		if t != nil {
-			t.Cells = append(t.Cells, s)
+		parentcell := m.Find("id", parentid, true)
+		if parentcell != nil {
+
+            var newpoints [][3]int
+            var parentcellright [3]int
+            var parentcellbottom [3]int
+            var parentcellleft [3]int
+            var parentcelltop [3]int
+            var newcellright [3]int
+            var newcellbottom [3]int
+            var newcellleft [3]int
+            var newcelltop [3]int
+
+            parentcellright[0] = parentcell.Position[0] + parentcell.Size[0]
+            parentcellright[1] = parentcell.Position[1] + parentcell.Size[1] / 2
+            parentcellbottom[0] = parentcell.Position[0] + parentcell.Size[0] / 2
+            parentcellbottom[1] = parentcell.Position[1] + parentcell.Size[1]
+            parentcellleft[0] = parentcell.Position[0]
+            parentcellleft[1] = parentcell.Position[1] + parentcell.Size[1] / 2
+            parentcelltop[0] = parentcell.Position[0] + parentcell.Size[0] / 2
+            parentcelltop[1] = parentcell.Position[1]
+
+            newcellright[0] = newcell.Position[0] + newcell.Size[0]
+            newcellright[1] = newcell.Position[1] + newcell.Size[1] / 2
+            newcellbottom[0] = newcell.Position[0] + newcell.Size[0] / 2
+            newcellbottom[1] = newcell.Position[1] + newcell.Size[1]
+            newcellleft[0] = newcell.Position[0]
+            newcellleft[1] = newcell.Position[1] + newcell.Size[1] / 2
+            newcelltop[0] = newcell.Position[0] + newcell.Size[0] / 2
+            newcelltop[1] = newcell.Position[1]
+
+            if parentcellright[0] < newcellleft[0] {
+                //element on right
+                newpoints = append(newpoints, [3]int{newcellleft[0], newcellleft[1]}, [3]int{parentcellright[0], parentcellright[1]})
+            }else if parentcellbottom[1] < newcelltop[1] {
+                //element on bottom
+                newpoints = append(newpoints, [3]int{newcelltop[0], newcelltop[1]}, [3]int{parentcellbottom[0], parentcellbottom[1]})
+            }else if parentcellleft[0] > newcellright[0] {
+                //element on left
+                newpoints = append(newpoints, [3]int{newcellright[0], newcellright[1]}, [3]int{parentcellleft[0], parentcellleft[1]})
+            }else if parentcelltop[1] > newcellbottom[1] {
+                //element on top
+                newpoints = append(newpoints, [3]int{newcellbottom[0], newcellbottom[1]}, [3]int{parentcelltop[0], parentcelltop[1]})
+            }
+
+            if len(newpoints) > 0 {
+                newcell.Synapses = []Synapse{{
+                    Points: newpoints,
+                    Size: 1,
+                    Color: "red",
+                }}
+            }
+
+			parentcell.Cells = append(parentcell.Cells, newcell)
 		} else {
 			log.Println("not found " + parentid)
 		}
 	}
 
-	return s
+	return newcell
 }
